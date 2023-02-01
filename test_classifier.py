973695bffcec96fd3b017c271f4bf60fa6964c86
KeunhoByeon <bkh5922a@gmail.com>
2023-02-01 16:17:10 +0900

    주석 추가

--- test_classifier.py
+++ test_classifier.py
@@ -17,6 +17,7 @@ from utils import import_openslide, load_color_info
 openslide = import_openslide()
 
 
+# SVS 파일에서 thumbnail 이미지 생성
 def get_thumbnail(svs_path, thumbnail_size):
     slide = openslide.OpenSlide(svs_path)
     w_pixels, h_pixels = slide.level_dimensions[0]
@@ -31,6 +32,7 @@ def get_thumbnail(svs_path, thumbnail_size):
     return thumbnail, (w_pixels, h_pixels), ratio
 
 
+# 모델 출력 결과와 svs파일로 디버깅 이미지 생성
 def make_debug_image(outputs, svs_path, colors):
     # Load Thumbnail
     thumbnail, thumbnail_num_pixels, thumbnail_ratio = get_thumbnail(svs_path, 1024)
@@ -163,18 +165,18 @@ if __name__ == '__main__':
     # Arguments 설정
     parser = argparse.ArgumentParser(description='PyTorch Training')
     # Model Arguments
-    parser.add_argument('--model', default='efficientnet_b0') #[변경] 사용할 모델 이름
-    parser.add_argument('--num_classes', default=18, type=int, help='number of classes') #[변경] 데이터의 클래스 종류의 수
+    parser.add_argument('--model', default='efficientnet_b0')  # [변경] 사용할 모델 이름
+    parser.add_argument('--num_classes', default=18, type=int, help='number of classes')  # [변경] 데이터의 클래스 종류의 수
     parser.add_argument('--checkpoint', default=None, type=str, help='path to checkpoint, not necessary')
     parser.add_argument('--checkpoint_name', default='20230118191754', type=str)
     parser.add_argument('--checkpoint_epoch', default=100, type=int)
     # Data Arguments
-    parser.add_argument('--patch_data', default='./Data/Qupath2/patch', help='path to patch data') #[변경] 이미지 패치 저장 경로
-    parser.add_argument('--svs_data', default='./Data/Qupath2/data', help='path to svs data') #[변경] svs파일 저장 경로
-    parser.add_argument('--json_path', default='./Data/Qupath2/project/classifiers/classes.json', help='path to json file') #[변경] json파일 저장 경로
+    parser.add_argument('--patch_data', default='./Data/Qupath2/patch', help='path to patch data')  # [변경] 이미지 패치 저장 경로
+    parser.add_argument('--svs_data', default='./Data/Qupath2/data', help='path to svs data')  # [변경] svs파일 저장 경로
+    parser.add_argument('--json_path', default='./Data/Qupath2/project/classifiers/classes.json', help='path to json file')  # [변경] json파일 저장 경로
     parser.add_argument('--workers', default=4, type=int, help='number of data loading workers')
-    parser.add_argument('--input_size', default=512, type=int, help='image input size') #[변경] 입력 이미지의 크기
-    parser.add_argument('--batch_size', default=128, type=int, help='mini-batch size') #[변경] 배치 사이즈
+    parser.add_argument('--input_size', default=512, type=int, help='image input size')  # [변경] 입력 이미지의 크기
+    parser.add_argument('--batch_size', default=128, type=int, help='mini-batch size')  # [변경] 배치 사이즈
     # Validation and Debugging Arguments
     parser.add_argument('--print_confusion_mat', default=False, action='store_true')
     parser.add_argument('--patch_size', default=1024, type=int, help='num pixels of patch')
--- train_classifier.py
+++ train_classifier.py
@@ -14,30 +14,46 @@ from models import Classifier
 
 
 def val(epoch, model, criterion, val_loader, logger=None):
-    model.eval()
+    """
+    epoch: 학습할 에포크 횟수
+    model: 학습할 모델 객체
+    criterion: 손실 함수
+    val_loader: 평가 데이터를 담은 데이터 로더
+    logger: 로그를 저장할 객체
+    """
 
+    model.eval()  # 모델을 평가 모드로
+
+    # Confusion matrix 초기화
     confusion_mat = [[0 for _ in range(args.num_classes)] for _ in range(args.num_classes)]
+
+    # 평가 데이터를 가져오기 위한 반복문
     with torch.no_grad():  # Disable gradient calculation
         for i, (inputs, targets) in tqdm(enumerate(val_loader), leave=False, desc='Validation {}'.format(epoch), total=len(val_loader)):
-            # CUDA
+            # CUDA 사용 가능 시 GPU 사용
             if torch.cuda.is_available():
                 inputs = inputs.cuda()
                 targets = targets.cuda()
 
+            # 모델 출력
             output = model(inputs)
             loss = criterion(output, targets)
 
-            # Calculate Accuracy
+            # Accuracy 계산
             preds = torch.argmax(output, dim=1)
             acc = torch.sum(preds == targets).item() / len(inputs) * 100.
 
-            # Save history
+            # 로그 히스토리 저장
             logger.add_history('total', {'loss': loss.item(), 'accuracy': acc})
+
+            # Confusion matrix 저장
             for t, p in zip(targets, preds):
                 confusion_mat[int(t.item())][p.item()] += 1
 
+    # 히스토리 출력
     if logger is not None:
         logger('*Validation {}'.format(epoch), history_key='total', time=time.strftime('%Y.%m.%d.%H:%M:%S'))
+    # Confusion matrix 출력
     if args.print_confusion_mat:
         pd.set_option('display.max_rows', 500)
         pd.set_option('display.max_columns', 500)
@@ -46,39 +62,62 @@ def val(epoch, model, criterion, val_loader, logger=None):
 
 
 def train(args, epoch, model, criterion, optimizer, train_loader, logger=None):
-    model.train()
+    """
+    args: 학습 설정을 담고 있는 객체
+    epoch: 학습할 epoch 횟수
+    model: 학습할 모델 객체
+    criterion: 손실 함수
+    optimizer: 최적화 알고리즘
+    train_loader: 학습 데이터를 담은 데이터 로더
+    logger: 로그를 저장할 객체
+    """
+
+    model.train()  # 모델을 학습 모드로
 
     # For print progress
     num_progress, next_print = 0, args.print_freq
+
+    # Confusion matrix 초기화
     confusion_mat = [[0 for _ in range(args.num_classes)] for _ in range(args.num_classes)]
+
+    # 학습 데이터를 가져오기 위한 반복문
     for i, (inputs, targets) in enumerate(train_loader):
-        # CUDA
+        # CUDA 사용 가능 시 GPU 사용
         if torch.cuda.is_available():
             inputs = inputs.cuda()
             targets = targets.cuda()
 
+        # 경사 초기화
         optimizer.zero_grad()
+        # 모델 출력 값 계산
         output = model(inputs)
+        # 손실 계산
         loss = criterion(output, targets)
+        # 경사 계산
         loss.backward()
+        # 최적화
         optimizer.step()
 
-        # Calculate Accuracy
+        # 정확도 계산
         preds = torch.argmax(output, dim=1)
         acc = torch.sum(preds == targets).item() / len(inputs) * 100.
 
-        # Save history and Print log
+        # 로그 히스토리 저장
         num_progress += len(inputs)
         logger.add_history('total', {'loss': loss.item(), 'accuracy': acc})
         logger.add_history('batch', {'loss': loss.item(), 'accuracy': acc})
+
+        # Confusion matrix 저장
         for t, p in zip(targets, preds):
             confusion_mat[int(t.item())][p.item()] += 1
+
+        # 일정 주기마다 로그 히스토리 출력
         if num_progress >= next_print:
             if logger is not None:
                 logger(history_key='batch', epoch=epoch, batch=num_progress, time=time.strftime('%Y.%m.%d.%H:%M:%S'))
             next_print += args.print_freq
 
-    # Print log (total)
+    # 전체 로그 히스토리 및 Confusion matrix 출력
     if logger is not None:
         logger(history_key='total', epoch=epoch, lr=round(optimizer.param_groups[0]['lr'], 12))
     if args.print_confusion_mat:
@@ -148,24 +187,24 @@ if __name__ == '__main__':
     # Arguments 설정
     parser = argparse.ArgumentParser(description='PyTorch Training')
     # Model Arguments
-    parser.add_argument('--model', default='efficientnet_b0') #[변경] 사용할 모델 이름
-    parser.add_argument('--num_classes', default=18, type=int, help='number of classes') #[변경] 데이터의 클래스 종류의 수
+    parser.add_argument('--model', default='efficientnet_b0')  # [변경] 사용할 모델 이름
+    parser.add_argument('--num_classes', default=18, type=int, help='number of classes')  # [변경] 데이터의 클래스 종류의 수
     parser.add_argument('--pretrained', default=True, action='store_true', help='Load pretrained model.')
     parser.add_argument('--resume', default=None, type=str, help='path to latest checkpoint')
     # Data Arguments
-    parser.add_argument('--data', default='./Data/Qupath2/patch', help='path to dataset') #[변경] 이미지 패치 저장 경로
+    parser.add_argument('--data', default='./Data/Qupath2/patch', help='path to dataset')  # [변경] 이미지 패치 저장 경로
     parser.add_argument('--workers', default=4, type=int, help='number of data loading workers')
-    parser.add_argument('--input_size', default=512, type=int, help='image input size') #[변경] 입력 이미지의 크기
+    parser.add_argument('--input_size', default=512, type=int, help='image input size')  # [변경] 입력 이미지의 크기
     # Training Arguments
     parser.add_argument('--start_epoch', default=0, type=int, help='manual epoch number')
-    parser.add_argument('--epochs', default=300, type=int, help='number of total epochs to run') #[변경]훈련 반복 수
-    parser.add_argument('--batch_size', default=16, type=int, help='mini-batch size') #[변경]배치 사이즈
-    parser.add_argument('--lr', default=0.00001, type=float, help='initial learning rate', dest='lr') #[변경] 초기 Learning rate
+    parser.add_argument('--epochs', default=300, type=int, help='number of total epochs to run')  # [변경]훈련 반복 수
+    parser.add_argument('--batch_size', default=16, type=int, help='mini-batch size')  # [변경]배치 사이즈
+    parser.add_argument('--lr', default=0.00001, type=float, help='initial learning rate', dest='lr')  # [변경] 초기 Learning rate
     parser.add_argument('--seed', default=103, type=int, help='seed for initializing training.')
     # Validation and Debugging Arguments
     parser.add_argument('--val_freq', default=10, type=int, help='validation frequency')
     parser.add_argument('--print_freq', default=1000, type=int, help='print frequency')
-    parser.add_argument('--print_confusion_mat', default=False, action='store_true') #[변경] Validation이 끝날 때 Confusion Matrix 출력 여부.
+    parser.add_argument('--print_confusion_mat', default=False, action='store_true')  # [변경] Validation이 끝날 때 Confusion Matrix 출력 여부.
     parser.add_argument('--result', default='results', type=str, help='path to results')
     parser.add_argument('--tag', default=None, type=str)
     args = parser.parse_args()
